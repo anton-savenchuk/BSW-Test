48b4dc7325f9108515c106c4ffbf9bc4ad01e6bb
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2024-12-10 17:39:37 +0700

    Initialization project

---
+++ src/line_provider/src/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="A test service provider of event information.",
+    description="This is a very fancy FastAPI project, with auto docs for the API.",
+    version="0.1.0",
+    contact={
+        "name": "Anton Savenchuk",
+        "url": "https://github.com/anton-savenchuk",
+        "email": "savenchuk.dev@gmail.com",
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+)
+
+
+@app.get("/")
+def hello():
+    return {"message": "Hello, World!"}

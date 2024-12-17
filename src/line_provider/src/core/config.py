a83b8bdf03129212a89be6ccfcf54a7b97ee33b3
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2024-12-17 20:49:46 +0700

    feat(tasks): Update update_event_state task

--- src/line_provider/src/core/config.py
+++ src/line_provider/src/core/config.py
@@ -11,6 +11,8 @@ class Settings(BaseSettings):
     POSTGRES_HOST: str
     POSTGRES_PORT: int
 
+    TIMEDELTA: int
+
     @property
     def DATABASE_URL(self) -> PostgresDsn:
         return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
--- src/line_provider/src/tasks/tasks.py
+++ src/line_provider/src/tasks/tasks.py
@@ -2,9 +2,10 @@ import asyncio
 import json
 import random
 
-import aio_pika
+from aio_pika import Message
 
 from src.core.celery import celery_app
+from src.core.config import overall_settings
 from src.core.rabbitmq import async_rabbitmq_connection
 from src.events.schemas import (
     EventIDSchema,
@@ -34,18 +35,28 @@ async def complete_event_async(event_id: EventIDSchema):
             }
         )
     )
-    await update_event_state("event_states", event_id, event_new_state)
+    await update_event_state(event_id, event_new_state)
 
 
-async def update_event_state(queue_name: str, event_id: EventIDSchema, event_state: EventStateSchema):
-    async with async_rabbitmq_connection as connection:
-        message = {
-            "event_id": event_id,
-            "state": event_state,
-        }
+async def update_event_state(event_id: EventIDSchema, event_state: EventStateSchema):
+    message = {
+        "event_id": event_id,
+        "state": event_state,
+    }
 
-        await connection.declare_queue(queue_name, durable=True)
-        await connection.default_exchange.publish(
-            aio_pika.Message(body=json.dumps(message).encode()),
-            routing_key=queue_name,
+    async with async_rabbitmq_connection as channel:
+        await channel.declare_exchange(
+            overall_settings.RABBITMQ_EXCHANGE,
+            overall_settings.RABBITMQ_EXCHANGE_TYPE,
+            durable=True,
+        )
+        await channel.declare_queue(
+            overall_settings.RABBITMQ_QUEUE,
+            durable=True,
+        )
+        await channel.default_exchange.publish(
+            Message(
+                body=json.dumps(message).encode(),
+            ),
+            routing_key=overall_settings.RABBITMQ_QUEUE,
         )
